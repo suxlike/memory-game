faa40f4ed7335a1e7cab647f9b5b740a29b3e478
Umut Toker <utoker@gmail.com>
2021-07-31 09:43:15 -0400

    add game over and restart

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Grid, Paper, Typography } from "@material-ui/core";
+import { Grid, Typography } from "@material-ui/core";
 import { useState } from "react";
 import "./App.css";
 import Cards from "./Cards";
@@ -7,33 +7,29 @@ function App() {
   const [score, setScore] = useState({
     highScore: 0,
     currentScore: 0,
-    Clicked: false,
+    over: false,
   });
 
   return (
     <Grid container spacing={3} item xs={12} justifyContent="center">
       <Grid item xs={12}>
-        <Typography align="center" color="primary" variant="h1">
+        <Typography align="center" color="primary" variant="h3">
           {"Memory Game"}
         </Typography>
-        <Typography align="center" color="secondary" variant="h5">
+        <Typography align="center" color="secondary" variant="h6">
           {"Select a norse god that you have not selected. "}
         </Typography>
       </Grid>
       <Grid container spacing={3} item xs={12} justifyContent="center">
-        <Grid item xs={2}>
-          <Paper>
-            <Typography color="primary" variant="h6">
-              {"Current Score: " + score.currentScore}
-            </Typography>
-          </Paper>
+        <Grid item xs={1}>
+          <Typography color="primary" variant="h6">
+            {"Score: " + score.currentScore}
+          </Typography>
         </Grid>
-        <Grid item xs={2}>
-          <Paper>
-            <Typography color="primary" variant="h6">
-              {"High Score: " + score.highScore}
-            </Typography>
-          </Paper>
+        <Grid item xs={1}>
+          <Typography color="primary" variant="h6">
+            {"High Score: " + score.highScore}
+          </Typography>
         </Grid>
       </Grid>
       <Cards score={score} setScore={setScore} />
--- src/Cards.js
+++ src/Cards.js
@@ -5,6 +5,7 @@ import {
   CardMedia,
   CardContent,
   Typography,
+  Button,
 } from "@material-ui/core";
 import cards from "./cards";
 
@@ -12,51 +13,95 @@ const Cards = ({ setScore, score }) => {
   let shuffledCards = cards;
   return (
     <Grid container spacing={3} item xs={11}>
-      {shuffledCards.map((c) => (
-        <Grid item xs={2} key={c.id}>
-          <Card>
-            <CardActionArea
-              onClick={() => {
-                if (c.clicked === false) {
-                  c.clicked = true;
-                  let s = score.currentScore;
-                  setScore({ ...score, currentScore: s + 1 });
-                  for (let i = shuffledCards.length - 1; i > 0; i--) {
-                    const j = Math.floor(Math.random() * i);
-                    const temp = shuffledCards[i];
-                    shuffledCards[i] = shuffledCards[j];
-                    shuffledCards[j] = temp;
+      {!score.over &&
+        shuffledCards.map((c) => (
+          <Grid item xs={2} key={c.id}>
+            <Card>
+              <CardActionArea
+                onClick={() => {
+                  // switch (c.clicked) {
+                  //   case false && score.currentScore === score.highScore:
+                  //     c.clicked = true;
+                  //     let s = score.currentScore;
+                  //     setScore({ ...score, currentScore: s + 1 });
+                  //     for (let i = shuffledCards.length - 1; i > 0; i--) {
+                  //       const j = Math.floor(Math.random() * i);
+                  //       const temp = shuffledCards[i];
+                  //       shuffledCards[i] = shuffledCards[j];
+                  //       shuffledCards[j] = temp;
+                  //     }
+                  //     break;
+
+                  //   default:
+                  //     console.log("game over");
+                  //     break;
+                  // }
+                  if (
+                    c.clicked === false &&
+                    score.currentScore === score.highScore
+                  ) {
+                    c.clicked = true;
+                    let s = score.currentScore;
+                    setScore({
+                      ...score,
+                      currentScore: s + 1,
+                      highScore: s + 1,
+                    });
+                    for (let i = shuffledCards.length - 1; i > 0; i--) {
+                      const j = Math.floor(Math.random() * i);
+                      const temp = shuffledCards[i];
+                      shuffledCards[i] = shuffledCards[j];
+                      shuffledCards[j] = temp;
+                    }
+                  } else if (c.clicked === false) {
+                    c.clicked = true;
+                    let s = score.currentScore;
+                    setScore({ ...score, currentScore: s + 1 });
+                    for (let i = shuffledCards.length - 1; i > 0; i--) {
+                      const j = Math.floor(Math.random() * i);
+                      const temp = shuffledCards[i];
+                      shuffledCards[i] = shuffledCards[j];
+                      shuffledCards[j] = temp;
+                    }
+                  } else {
+                    setScore({ ...score, over: true });
+                    console.log("game over");
                   }
-                } else {
-                  console.log("game over");
-                }
-              }}
-            >
-              <CardMedia component="img" src={c.image} title={c.name} />
-              <CardContent>
-                <Typography
-                  gutterBottom
-                  align="center"
-                  variant="h5"
-                  component="h2"
-                >
-                  {c.name}
-                </Typography>
-                <Typography
-                  align="center"
-                  variant="body2"
-                  color="textSecondary"
-                  component="p"
-                >
-                  {c.description}
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-          </Card>
-        </Grid>
-      ))}
+                }}
+              >
+                <CardMedia component="img" src={c.image} title={c.name} />
+                <CardContent>
+                  <Typography
+                    gutterBottom
+                    align="center"
+                    variant="h5"
+                    component="h2"
+                  >
+                    {c.name}
+                  </Typography>
+                  <Typography
+                    align="center"
+                    variant="body2"
+                    color="textSecondary"
+                    component="p"
+                  >
+                    {c.description}
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+          </Grid>
+        ))}
+      {score.over && (
+        <Button
+          onClick={() => setScore({ ...score, currentScore: 0, over: false })}
+        >
+          Restart
+        </Button>
+      )}
     </Grid>
   );
 };
 
 export default Cards;
+  
